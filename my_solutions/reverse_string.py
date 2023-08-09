a2c2e2e4ba2c8f309d7921275518299ec12abd0d
marcoan01 <57053359+marcoan01@users.noreply.github.com>
2023-08-09 12:08:39 -0500

    Stack class defined with a challenge of reversing a string variable using Stacks

--- my_solutions/reverse_string.py
+++ my_solutions/reverse_string.py
@@ -11,5 +11,11 @@ reversed_string = ""
 s = stack.Stack()
 
 # Your solution here.
+for char in string:
+    s.push(char)
+
+while s.isEmpty() != True:
+    reversed_string = reversed_string + s.pop()
 
 print(reversed_string)
+
