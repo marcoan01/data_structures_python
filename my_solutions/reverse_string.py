55e1c23f1f3ad48d9436ecde8443930479eb613c
marcoan01 <57053359+marcoan01@users.noreply.github.com>
2023-08-09 12:11:25 -0500

    removing boilerplate txt

--- my_solutions/reverse_string.py
+++ my_solutions/reverse_string.py
@@ -1,9 +1,3 @@
-"""
-Python Data Structures - A Game-Based Approach
-Stack challenge
-Robin Andrews - https://compucademy.net/
-"""
-
 import stack
 
 string = "gninraeL nIdekniL htiw tol a nraeL"
--- my_solutions/stack.py
+++ my_solutions/stack.py
@@ -1,10 +1,3 @@
-"""
-Python Data Structures - A Game-Based Approach
-Stack class
-Robin Andrews - https://compucademy.net/
-"""
-
-
 class Stack:
     def __init__(self):
         self.items = []
